ab06e840ae87bdb56684698b804fc5fc4bdb2b9e
PuaR0 <92799139+PuaR0@users.noreply.github.com>
2022-06-23 08:19:25 +0300

    Initial Commit

---
+++ Randomiser2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Randomiser2
+//
+//  Created by Алиага С on 23.6.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
